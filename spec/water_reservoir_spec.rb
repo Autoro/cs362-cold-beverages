442538331ee5ced616a3e02d9245ea576cb37406
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-19 15:43:33 -0700

    Added test for water reservoir starting empty.

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -7,4 +7,10 @@ describe 'A water reservoir' do
     expect(reservoir.capacity).to eq(10)
     expect(reservoir.current_water_volume).to eq(10)
   end
+
+  it "is initially empty" do
+    reservoir = WaterReservoir.new
+
+    expect(reservoir).to be_empty
+  end
 end
