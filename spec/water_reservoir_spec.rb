11ce60755873a4903a82949d4dc361899188d757
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-19 15:42:31 -0700

    Added test for water reservoir capacity and current volume.

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+  it "has a capacity and current volume" do
+    reservoir = WaterReservoir.new(10, 10)
 
+    expect(reservoir.capacity).to eq(10)
+    expect(reservoir.current_water_volume).to eq(10)
+  end
 end
