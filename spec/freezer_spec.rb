45f4ff5a139a6a27f7f05f553c321d5364dbde0d
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-19 16:28:02 -0700

    Added test for freezer capacity and temperature.

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
+  let(:freezer) { Freezer.new(100) }
 
+  it "has a capacity and temperature" do
+    expect(freezer.capacity).to eq(100)
+    expect(freezer.temperature).to eq(70)
+  end
 end
