4ff03ad25234eed4c1fcefe19429007a4d90b9d1
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-19 15:35:07 -0700

    Added test for water dispenser having a reservoir.

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../lib/water_dispenser'
+require_relative '../lib/water_reservoir'
 
 describe 'A water dispenser' do
+  it "has a reservoir" do
+    reservoir = WaterReservoir.new
+    dispenser = WaterDispenser.new(reservoir)
 
+    expect(dispenser.reservoir).to_not be_nil
+  end
 end
