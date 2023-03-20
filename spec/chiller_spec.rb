18192e3524d203790f212eaab35df5cb68b25258
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-19 17:19:10 -0700

    Added test for turning on a chiller.

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -1,10 +1,18 @@
 require_relative '../lib/chiller'
 
+class TestChiller < Chiller
+  attr_reader :power
+end
+
 describe 'A chiller' do
-  let(:chiller) { Chiller.new(100) }
+  let(:chiller) { TestChiller.new(100) }
 
   it "has a capacity and temperature" do
     expect(chiller.capacity).to eq(100)
     expect(chiller.temperature).to eq(70)
+  end
+
+  it "can be turned on" do
+    expect { chiller.turn_on }.to change(chiller, :power).from(:off).to(:on)
   end
 end
