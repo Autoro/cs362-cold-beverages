93857e915b2dca7b833178ea1906f8e6f0ad955b
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-19 17:02:25 -0700

    Added test for adding an item to freezer.

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,11 +1,12 @@
 require_relative '../lib/freezer'
 
 class TestFreezer < Freezer
-  attr_reader :power
+  attr_reader :power, :contents
 end
 
 describe 'A freezer' do
   let(:freezer) { TestFreezer.new(100) }
+  let(:item) { Item.new("FAKE", 5) }
 
   it "has a capacity and temperature" do
     expect(freezer.capacity).to eq(100)
@@ -19,5 +20,10 @@ describe 'A freezer' do
   it "can be turned off" do
     freezer.turn_on
     expect { freezer.turn_off }.to change(freezer, :power).from(:on).to(:off)
+  end
+
+  it "can add an item" do
+    freezer.add(item)
+    expect(freezer.contents).to include(item)
   end
 end
