826b92177df3ad1f75094e579747a95b03e1e1f7
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-19 21:33:32 -0700

    Added test for a refrigerator chilling an item.

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -1,16 +1,23 @@
 require_relative '../lib/refrigerator'
+require_relative '../lib/fake_chiller'
 
 describe 'A refrigerator' do
-  let(:chiller) { Chiller.new(100) }
+  let(:chiller) { FakeChiller.new(100) }
   let(:freezer) { Freezer.new(100) }
   let(:reservoir) { WaterReservoir.new }
   let(:dispenser) { WaterDispenser.new(reservoir) }
   let(:refrigerator) { Refrigerator.new(chiller, freezer, dispenser, reservoir) }
+  let(:item) { Item.new("FAKE", 5) }
 
   it "has a chiller, freezer, dispenser, and reservoir" do
     expect(refrigerator.chiller).to_not be_nil
     expect(refrigerator.freezer).to_not be_nil
     expect(refrigerator.water_dispenser).to_not be_nil
     expect(refrigerator.water_reservoir).to_not be_nil
+  end
+
+  it "can chill an item" do
+    refrigerator.chill(item)
+    expect(refrigerator.chiller.contents).to include(item)
   end
 end
