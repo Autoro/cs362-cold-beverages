ff48fbeb262c5c205b7d1b6f482a6bde1b74dadc
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-19 17:34:06 -0700

    Added test for refrigerator having a chiller, freezer, dispenser, and reservoir.

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -1,5 +1,16 @@
 require_relative '../lib/refrigerator'
 
 describe 'A refrigerator' do
+  let(:chiller) { Chiller.new(100) }
+  let(:freezer) { Freezer.new(100) }
+  let(:reservoir) { WaterReservoir.new }
+  let(:dispenser) { WaterDispenser.new(reservoir) }
+  let(:refrigerator) { Refrigerator.new(chiller, freezer, dispenser, reservoir) }
 
+  it "has a chiller, freezer, dispenser, and reservoir" do
+    expect(refrigerator.chiller).to_not be_nil
+    expect(refrigerator.freezer).to_not be_nil
+    expect(refrigerator.water_dispenser).to_not be_nil
+    expect(refrigerator.water_reservoir).to_not be_nil
+  end
 end
